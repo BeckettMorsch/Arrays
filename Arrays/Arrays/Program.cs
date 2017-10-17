ab29b7cc8cdfb4e958b32851132b7561812f2f2a
BeckettMorsch <beckettmorsch@gmail.com>
2017-10-17 11:33:46 -0400

    made arrays

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] w = new String[100];
+
+            for(int i= 0; i < 100;i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                    w[i] = i + ". " + "FizzBuzz";
+
+                else if (i % 3 == 0)
+                    w[i] = i + ". " + "Fizz";
+
+                else if (i % 5 == 0)
+                    w[i] = i + ". " + "Buzz";
+                else
+                    w[i] = i + ". ";
+
+            }
+            for(int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(w[i]);
+            }
+            
+
+
+        }
+    }
+}
---
+++ Arrays/Unique digits/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unique_digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random(System.Environment.TickCount);
+            rand.Next(0, 51);
+            int[] n = new int[50];
+        
+            for(int i=0;i<50;i++)
+            {
+                n[i] = rand.Next(0, 51);
+            }
+
+            for (int i = 0; i < 50; i++)
+            {
+                if(n[i]== )
+            }
+
+
+
+
+        }
+    }
+}
