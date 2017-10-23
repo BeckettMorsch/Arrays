ddbaa1473d1c242faf9ba574008d2558de9580bd
BeckettMorsch <beckettmorsch@gmail.com>
2017-10-23 11:32:01 -0400

    finished minmax and almost finished dedupe

---
+++ Arrays/Dedupe/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dedupe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[50];
+
+            Random rand = new Random();
+            int lowest = 10000;
+            int highest = 0;
+            int removed = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+                nums[i] = rand.Next(0, 51);
+                
+            }
+
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for(int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        //nums[i] = -1;
+                        
+                        removed++;
+                    }
+                }
+            }
+            Array.Sort(nums);
+
+            for (int i=0;i<nums.Length;i++)
+            Console.Write(nums[i] + ", ");
+            Console.WriteLine("");
+
+            int final = nums.Length - removed;
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Arrays/MinMax/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                    System.Console.WriteLine("Min max");
+
+                    int[] nums = new int[50];
+
+                    Random rand = new Random();
+                    int lowest = 10000;
+                    int highest = 0;
+                    for (int i = 0; i < nums.Length; i++)
+                    {
+
+                        nums[i] = rand.Next(0, 51);
+                        // Console.Write(nums[i] + ",");
+                    }
+
+
+                    for (int i = 0; i < nums.Length; i++)
+                    {
+                        if (nums[i] < lowest)
+                            lowest = nums[i];
+
+                    }
+
+
+                    for (int i = 0; i < nums.Length; i++)
+                    {
+                        if (nums[i] > highest)
+                            highest = nums[i];
+                    }
+
+
+
+
+
+
+                    for (int i = 0; i < nums.Length; i++)
+                    {
+                        if (i % 10 == 0)
+                        {
+                            Console.WriteLine("");
+                        }
+
+                        if (i != 49)
+                            Console.Write(nums[i] + ",");
+                        else
+                            Console.Write(nums[i] + " \n  \n");
+                    }
+                    Console.WriteLine("min: " + lowest);
+                    Console.WriteLine("max: " + highest);
+
+
+                
+            
+        }
+    }
+}
