a7dff62fe2d4ddfdb845c127623c7f6cd0c74750
BeckettMorsch <beckettmorsch@gmail.com>
2017-10-25 11:03:03 -0400

    Started tic tac toe

---
+++ Arrays/Tic Tac Toe/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[,] slots = new String[2,2];
+
+
+
+            Console.WriteLine("    A   B   C  ");
+            Console.WriteLine("  +---+---+---+");
+            Console.WriteLine("1 |   |   |   |");
+            Console.WriteLine("  +---+---+---+");
+            Console.WriteLine("2 |   |   |   |");
+            Console.WriteLine("  +---+---+---+");
+            Console.WriteLine("3 |   |   |   |");
+            Console.WriteLine("  +---+---+---+");
+
+        }
+    }
+}
