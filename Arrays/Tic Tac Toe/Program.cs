ac7846357968e808493d32b28bd9f5555c4312c4
BeckettMorsch <beckettmorsch@gmail.com>
2017-10-27 11:32:53 -0400

    safety save

--- Arrays/Tic Tac Toe/Program.cs
+++ Arrays/Tic Tac Toe/Program.cs
@@ -15,7 +15,7 @@ namespace Tic_Tac_Toe
             bool win = false;
             bool draw = false;
             String move;
-            bool check = true;
+            bool check = false;
             int row, col;
             String there = "";
             String first;
@@ -31,16 +31,17 @@ namespace Tic_Tac_Toe
             Console.WriteLine("3 |   |   |   |");
             Console.WriteLine("  +---+---+---+");
 
-            Console.WriteLine("    A   B   C  ");
-            Console.WriteLine("  +---+---+---+");
-            Console.WriteLine("1 | " + slots[0, 0] + "  | " + slots[0, 1] + "  | " + slots[0, 2] + "  |");
-            Console.WriteLine("  +---+---+---+");
-            Console.WriteLine("2 | " + slots[1, 0] + "  | " + slots[1, 1] + "  | " + slots[1, 2] + "  |");
-            Console.WriteLine("  +---+---+---+");
-            Console.WriteLine("1 | " + slots[2, 0] + "  | " + slots[2, 1] + "  | " + slots[2, 2] + "  |");
-            Console.WriteLine("  +---+---+---+");
+            
 
-            while (check != false) 
+            //Space out the board
+            for(int i=0;i<3;i++)
+            {
+                for(int j=0;j<3;j++)
+                   slots[i, j] = "   ";
+            }
+
+            //TURN 1
+            while (check == false && win == false) 
             {
                 
                 Console.WriteLine("Player X, where do you want to move? : ");
@@ -61,30 +62,86 @@ namespace Tic_Tac_Toe
                     col = 2;
                 row = Convert.ToInt32(move.Substring(0, 1)) -1;
 
-                // checks if something is there
-                if (slots[row,col] != "")
+                /*/ checks if something is there
+                if (slots[row,col] != " ")
                 {
                     valid = false;
-                }
+                }/*/
 
                 // Puts X into board
-                if(valid == true)
+                if (valid == true)
                 {
-                    slots[row, col] = "X";
+                    slots[row, col] = " X ";
+
+                    check = true;
+                    Console.WriteLine("    A   B   C  ");
+                    Console.WriteLine("  +---+---+---+");
+                    Console.WriteLine("1 |" + slots[0, 0] + "|" + slots[0, 1] + "|" + slots[0, 2] + "|");
+                    Console.WriteLine("  +---+---+---+");
+                    Console.WriteLine("2 |" + slots[1, 0] + "|" + slots[1, 1] + "|" + slots[1, 2] + "|");
+                    Console.WriteLine("  +---+---+---+");
+                    Console.WriteLine("1 |" + slots[2, 0] + "|" + slots[2, 1] + "|" + slots[2, 2] + "|");
+                    Console.WriteLine("  +---+---+---+");
+
                 }
-
-
-
-
-
-
-
-
-
-
+                else
+                {
+                    check = false;
+                    Console.WriteLine("That is an invalid move. Try again");
+                }                
             }
 
+           // check = true;
+            // TURN 2
+            while (check == false && win == false)
+            {
 
+                Console.WriteLine("Player O, where do you want to move? : ");
+                move = Console.ReadLine();
+
+                // checks if on the board
+                if (move != "1A" && move != "1B" && move != "1C" && move != "2A" && move != "2B" && move != "2C" && move != "3A" && move != "3B" && move != "3C")
+                    valid = false;
+                else
+                    valid = true;
+
+                //Assigns row and col
+                if (move.Substring(1, 1) == "A")
+                    col = 0;
+                else if (move.Substring(1, 1) == "B")
+                    col = 1;
+                else
+                    col = 2;
+                row = Convert.ToInt32(move.Substring(0, 1)) - 1;
+
+                /*/ checks if something is there
+                if (slots[row,col] != " ")
+                {
+                    valid = false;
+                }/*/
+
+                // Puts X into board
+                if (valid == true)
+                {
+                    slots[row, col] = " O ";
+
+                    check = true;
+                    Console.WriteLine("    A   B   C  ");
+                    Console.WriteLine("  +---+---+---+");
+                    Console.WriteLine("1 |" + slots[0, 0] + "|" + slots[0, 1] + "|" + slots[0, 2] + "|");
+                    Console.WriteLine("  +---+---+---+");
+                    Console.WriteLine("2 |" + slots[1, 0] + "|" + slots[1, 1] + "|" + slots[1, 2] + "|");
+                    Console.WriteLine("  +---+---+---+");
+                    Console.WriteLine("1 |" + slots[2, 0] + "|" + slots[2, 1] + "|" + slots[2, 2] + "|");
+                    Console.WriteLine("  +---+---+---+");
+
+                }
+                else
+                {
+                    check = false;
+                    Console.WriteLine("That is an invalid move. Try again");
+                }
+            }
 
 
         }
